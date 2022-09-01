50ce158cec8179e8629873ea3a9e743d87e92996
Ivelina Balcheva <ivab1414@gmail.com>
2022-09-01 23:23:05 +0300

    Configuration

---
+++ config.py
@@ -0,0 +1,46 @@
+from decouple import config
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from db import db
+from resources.routes import routes
+
+
+class ProductionConfig:
+    FLASK_ENV = "prod"
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}"
+        f"@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
+    )
+
+
+class DevelopmentConfig:
+    FLASK_ENV = "development"
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}"
+        f"@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
+    )
+
+
+class TestingConfig:
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = f"postgresql:{config('DB_PASSWORD')}//postgres:@localhost:5432/test_db"
+    DEBUG = True
+
+
+def create_app(config='config.DevelopmentConfig'):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    api = Api(app)
+    migrate = Migrate(app, db)
+    CORS(app)
+    [api.add_resource(*route) for route in routes]
+    return app
